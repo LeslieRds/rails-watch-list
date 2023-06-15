eb46630eb5cb2a0e05960aeb29eb56fd57ead3f3
Leslie Rados <leslie.rados@gmail.com>
2023-06-15 14:21:05 +0200

    tsil hctaw sliar routes, controleur et vues done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "lists", to: "lists#index", as: :lists
+  # get "lists/new", to: "lists#new", as: :new_list
+  # get "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: :list
+  resources :bookmarks, :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
